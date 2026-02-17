15bed57a083ee82a8423a8b211574a3fc3411eb9
Emilly Paiva <emillypaiva3260@gmail.com>
2026-02-17 10:12:47 -0300

    :recycle: refactor: wire up user profile endpoints in controller and module

--- src/modules/identity/identity.module.ts
+++ src/modules/identity/identity.module.ts
@@ -1,15 +1,25 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '@shared/infra/prisma.service';
-import { RegisterUserUseCase } from './application/use-cases/register-user.use-case';
+
 import { UsersRepository } from './domain/repositories/users-repository.interface';
 import { PrismaUsersRepository } from './infra/database/prisma/repositories/prisma-users.repository';
+
+import { RegisterUserUseCase } from './application/use-cases/register-user.use-case';
+import { GetUserProfileUseCase } from './application/use-cases/get-user-profile.use-case';
+import { UpdateUserUseCase } from './application/use-cases/update-user.use-case';
+import { DeleteUserUseCase } from './application/use-cases/delete-user.use-case';
+
 import { RegisterUserController } from './presentation/controllers/register-user.controller';
+import { UserProfileController } from './presentation/controllers/user-profile.controller';
 
 @Module({
-  controllers: [RegisterUserController],
+  controllers: [RegisterUserController, UserProfileController],
   providers: [
     PrismaService,
     RegisterUserUseCase,
+    GetUserProfileUseCase,
+    UpdateUserUseCase,
+    DeleteUserUseCase,
     {
       provide: UsersRepository,
       useClass: PrismaUsersRepository,
