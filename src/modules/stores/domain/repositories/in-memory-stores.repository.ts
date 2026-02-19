326bfebfcc0d5decc8b0ab1674c438062e27ca0f
Emilly Paiva <emillypaiva3260@gmail.com>
2026-02-19 20:13:53 -0300

    test(stores): adicionando in-memory repository e testes pro create use case

---
+++ src/modules/stores/domain/repositories/in-memory-stores.repository.ts
@@ -0,0 +1,34 @@
+import { StoresRepository } from './stores-repository.interface';
+import { Store } from '../entities/store.entity';
+
+export class InMemoryStoresRepository implements StoresRepository {
+  public items: Store[] = [];
+
+  create(store: Store): Promise<void> {
+    this.items.push(store);
+    return Promise.resolve();
+  }
+
+  findById(id: string): Promise<Store | null> {
+    const store = this.items.find((item) => item.id === id);
+    return Promise.resolve(store || null);
+  }
+
+  findByOwnerId(ownerId: string): Promise<Store | null> {
+    const store = this.items.find((item) => item.ownerId === ownerId);
+    return Promise.resolve(store || null);
+  }
+
+  findByName(name: string): Promise<Store | null> {
+    const store = this.items.find((item) => item.name === name);
+    return Promise.resolve(store || null);
+  }
+
+  save(store: Store): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === store.id);
+    if (index !== -1) {
+      this.items[index] = store;
+    }
+    return Promise.resolve();
+  }
+}
