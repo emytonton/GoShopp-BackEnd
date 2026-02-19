3212da993ee7a5d2e23a04073d7ad3afb912141c
Emilly Paiva <emillypaiva3260@gmail.com>
2026-02-19 19:27:21 -0300

    :building_construction: feat(stores): criando contrato do repositorio de lojas

---
+++ src/modules/stores/domain/repositories/stores-repository.interface.ts
@@ -0,0 +1,9 @@
+import { Store } from '../entities/store.entity';
+
+export abstract class StoresRepository {
+  abstract create(store: Store): Promise<void>;
+  abstract findById(id: string): Promise<Store | null>;
+  abstract findByOwnerId(ownerId: string): Promise<Store | null>;
+  abstract findByName(name: string): Promise<Store | null>;
+  abstract save(store: Store): Promise<void>;
+}
